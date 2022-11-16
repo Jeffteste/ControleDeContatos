e54b057e9b33f6ca52671dd19a1296d938c8db37
jeff <jeffersonlacerda.r@gmail.com>
2022-11-16 10:28:02 -0300

    1

--- ControleDeContatos/Models/UsuarioModel.cs
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -6,7 +6,7 @@ namespace ControleDeContatos.Models
     public class UsuarioModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        public string Nome { get; set; }    
         public string Login { get; set; }
         public string Email { get; set; }
         public PerfilEnum Perfil { get; set;} 
